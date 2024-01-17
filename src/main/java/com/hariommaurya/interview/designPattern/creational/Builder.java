4d64a44c428527eecd7fe34104757d5e9558bd1a
harrymauray05 <harrymaurya05@gmail.com>
2024-01-18 00:03:39 +0530

    student class with nested builder pattern

--- src/main/java/com/hariommaurya/interview/designPattern/creational/Builder.java
+++ src/main/java/com/hariommaurya/interview/designPattern/creational/Builder.java
@@ -1,7 +1,10 @@
 package com.hariommaurya.interview.designPattern.creational;
 
 
+import java.util.*;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class Eployee{
     private String name;
@@ -147,9 +150,6 @@ class Order{
         private String code;
         private Integer price;
         Builder(){}
-
-
-
         public Builder id(String id) {
             this.id = id;
             return this;
@@ -170,6 +170,155 @@ class Order{
         }
     }
 }
+enum SubjectName{
+    ENGLISH,MATH,SCIENCE;
+}
+class Subject{
+    private float marks;
+    private SubjectName subjectName;
+
+    public Subject(Builder builder){
+        setMarks(builder.marks);
+        setSubjectName(builder.subjectName);
+    }
+
+    public float marks() {
+        return marks;
+    }
+
+    public Subject setMarks(float marks) {
+        this.marks = marks;
+        return this;
+    }
+
+    public SubjectName subjectName() {
+        return subjectName;
+    }
+
+    public Subject setSubjectName(SubjectName subjectName) {
+        this.subjectName = subjectName;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Subject{" +
+                "marks=" + marks +
+                ", subjectName=" + subjectName +
+                '}';
+    }
+
+    public static class Builder{
+        private float marks;
+        private SubjectName subjectName;
+
+        public float marks() {
+            return marks;
+        }
+
+        public Builder setMarks(float marks) {
+            this.marks = marks;
+            return this;
+        }
+
+        public SubjectName subjectName() {
+            return subjectName;
+        }
+
+        public Builder setSubjectName(SubjectName subjectName) {
+            this.subjectName = subjectName;
+            return this;
+        }
+        public Subject build(){
+            return new Subject(this);
+        }
+    }
+}
+class Student{
+    private String name;
+    private int rollNumber;
+    private Subject subject;
+
+    public Student(){}
+
+    public Student(Builder builder){
+        setName(builder.name);
+        setSubject(builder.subject);
+        setRollNumber(builder.rollNumber);
+    }
+    public String name() {
+        return name;
+    }
+
+    public Student setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public int rollNumber() {
+        return rollNumber;
+    }
+
+    public Student setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+        return this;
+    }
+
+    public Subject subject() {
+        return subject;
+    }
+
+    public Student setSubject(Subject subject) {
+        this.subject = subject;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", rollNumber=" + rollNumber +
+                ", subject=" + subject +
+                '}';
+    }
+
+    public static  class Builder{
+        private String name;
+        private int rollNumber;
+        private Subject subject;
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setRollNumber(int rollNumber) {
+            this.rollNumber = rollNumber;
+            return this;
+        }
+
+        public Builder setSubject(Subject subject) {
+            this.subject = subject;
+            return this;
+        }
+
+        public String name() {
+            return name;
+        }
+
+        public int rollNumber() {
+            return rollNumber;
+        }
+
+        public Subject subject() {
+            return subject;
+        }
+
+        public Student build(){
+            return  new Student(this);
+        }
+    }
+}
 public class Builder {
     public static void main(String[] args) {
         Order order = new Order.Builder().code("mobile").price(423).id("233").build();
@@ -178,5 +327,16 @@ public class Builder {
         Order order1 = new Order.Builder().code("mobile1").price(42323).id("233").build();
         Eployee eployee = new Eployee.Builder().id(1).name("Hariom").salary(1000.0).build();
         System.out.println(eployee);
+
+        Student student = new Student.Builder()
+                .setName("Hariom")
+                .setRollNumber(21)
+                .setSubject(new Subject.Builder().setSubjectName(SubjectName.MATH).setMarks(10).build())
+                .build();
+
+        System.out.println(student);
+
+
+
     }
 }
